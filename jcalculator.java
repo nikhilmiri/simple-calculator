720d08ba07bd2e98fe65f19e41000ebc3761ed2b
nikhilmiri <112744897+nikhilmiri@users.noreply.github.com>
2022-09-03 14:57:44 +0530

    Update and rename jcalculator to jcalculator.java

---
+++ jcalculator.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.lang.*;
+import java.lang.Math;
+import java.util.Scanner;
+public class jcalculator {
+  
+    public static void main(String[] args)
+    {
+        int a =20,b=5;
+      
+      System.out.println("additon of two num : " (a+b));
+      
+      System.out.println("subtract of two num : " (a-b));
+      
+      System.out.println("multiplication of two num : " (a*b));
+      
+      System.out.println("division of two num : " (a/b));
+      
+    }
+}
